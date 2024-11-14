978f411d5e4c09b52e9db928e0ac9d200c67e715
tesfainnovation <tesfainnovation@gmail.com>
2024-11-14 11:53:20 -0600

    axios and requests

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import Home from './pages/home/Home';
+import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-        <Home/>
+      <Home />
     </div>
   );
 }
